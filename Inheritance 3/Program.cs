903537029ab4b8ac3363237f18049674c3a2905a
yankovich.olga24 <yankovich.olga24@gmail.com>
2019-07-06 17:39:54 +0300

    Добавьте файлы проекта.

---
+++ Inheritance 3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Inheritance_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SymbArray symbArray = new SymbArray(new int[] { 1, 6, 3, 7, 4 }, new char[] {'a','s','d','f'});
+            Console.WriteLine(symbArray.ToString());
+            Console.ReadKey();
+        }
+    }
+
+
+    class NumArray
+    {
+        public int[] numarray;
+
+        public NumArray(int[] _array)
+        {
+            numarray = _array;
+        }
+
+        public override string ToString()
+        {
+            string txt="";
+            foreach (int i in numarray)
+            {
+                Convert.ToString(i);
+                txt += i;
+            }         
+            return txt ;
+        }
+    }
+
+    class SymbArray:NumArray
+    {
+        public char[] symbArray;
+
+        public SymbArray(int[] numArray, char[] symbArray) : base(numArray)
+        {
+            this.symbArray = symbArray;
+        }
+
+        public override string ToString()
+        {
+            string firstTxt = "";
+            foreach (int i in numarray)
+            {
+                Convert.ToString(i);
+                firstTxt += i;
+            }
+            string secondtTxt="";
+            foreach (char i in symbArray)
+            {
+                Convert.ToString(i);
+                secondtTxt += i;
+            }
+            string summaryText=(firstTxt+"   "+secondtTxt);
+
+            return summaryText;
+        }
+    }
+
+}
